715caaae46a11635da809f51f1214c389eb5a39a
satish-nirmal <satishnirmal16@gmail.com>
2023-02-15 23:58:46 +0530

    Its my first project to example for git

---
+++ Identifier.java
@@ -0,0 +1,12 @@
+package com.java.identifier;
+
+public class Identifier {  // class name is a 1st indentifier
+
+	public static void main(String[] args) { // mian is 2nd and string is 3rd and array is 4th identifier
+		
+		int x = 10;  // x is 5 identifier
+		
+		System.out.println(x);
+	}
+
+}
---
+++ Naming_Convension.java
@@ -0,0 +1,16 @@
+package com.java.identifier;
+
+public class Naming_Convension {
+
+	
+	public static void main(String[] args) {
+		
+		int String = 10 ; 
+		                        // This naming convension is not good programming practice
+		int Runable = 20 ;
+		
+		System.out.println(String);
+		
+		System.out.println(Runable);
+	}
+}
